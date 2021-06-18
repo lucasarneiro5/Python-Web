9f0f0ce8d928a4ab90262fc68c03a12a85441ef2
Lucas Arneiro <lucasarneiro.v@gmail.com>
2021-06-17 21:16:39 -0300

    Fixing WhatsApp Robot

--- Selenium/WhatsApp_Auto.py
+++ Selenium/WhatsApp_Auto.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 import time
 
 
+
 class WhatsappBot:
     def __init__(self):
-        self.mensagem = 'Oi amor, te amo!'
+        self.mensagem = 'This is robot. I´m learning Selenium!'
         self.grupos = ["Amor <3"]
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
@@ -13,19 +14,19 @@ class WhatsappBot:
 
     def SendMessenge(self):
         #HTML tag from grupo: //*[@id="pane-side"]/div[1]/div/div/div[10]/div/div/div[2]/div[1]/div[1]/span/span
-        #Caixa de texto: //*[@id="main"]/footer/div[1]/div[2]/div/div[2]
-        #Botao de Enviar: //*[@id="main"]/footer/div[1]/div[3]
+        #Caixa de texto: <div tabindex="-1" class="_2A8P4">
+        #Botao de Enviar: <span data-testid="send" data-icon="send" class="">
         self.browser.get('https://web.whatsapp.com/')
         time.sleep(30)
         for grupo in self.grupos:
             grupo = self.browser.find_element_by_xpath(f"//span[@title='{grupo}']")
             time.sleep(3)
             grupo.click()
-            chat_box = self.browser.find_element_by_class_name('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+            chat_box = self.browser.find_element_by_class_name('_2A8P4')
             time.sleep(3)
             chat_box.click()
             chat_box.send_keys(self.mensagem)
-            send_button = self.browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]')
+            send_button = self.browser.find_element_by_xpath("//span[@data-icon='send']")
             time.sleep(3)
             send_button.click()
 
